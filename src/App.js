a838354d7a348e24b56318d35e094c99244f7087
YAREMAG <jeremy.ganushchak@gmail.com>
2024-10-30 14:50:42 -0400

    Removed birds

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from './components/Home';
 import About from './components/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
-import Birds from './components/Birds';
+
 import Charts from './components/Charts';
 import CV from './components/CV';
 
@@ -15,8 +15,8 @@ function App() {
       <div className="App">
         <header className="App-header">
           <nav>
-            <Link to="/home">Home</Link> | <Link to="/about">About Me</Link> | <Link to="/projects">Projects</Link> | <Link to="/contact">Contact</Link> | <Link to="/birds">Birds </Link> 
-            | <Link to="/charts">Charts</Link> | <Link to="/cv">CV</Link>
+            <Link to="/home">Home</Link> | <Link to="/about">About Me</Link> | <Link to="/projects">Projects</Link> | <Link to="/contact">Contact</Link> |
+           <Link to="/charts">Charts</Link> | <Link to="/cv">CV</Link>
           </nav>
         </header>
         <Routes>
@@ -25,7 +25,6 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/contact" element={<Contact />} />
-          <Route path="/birds" element={<Birds/>}/>
           <Route path="/charts" element={<Charts/>}/>
           <Route path="/cv" element={<CV/>}/>
         </Routes>
